3b91b85ea20f0de31b1b23a19c5d99643a9d67d1
Arunraj Velmurugan <arunrajvel02@gmail.com>
2026-06-12 11:09:42 +0530

    Create Students and Employees table queries

---
+++ homework2.sql
@@ -0,0 +1,6 @@
+
+Alter Table Student 
+Add Column email VARCHAR(100) UNIQUE,
+Add Column phone_Number VARCHAR(15) UNIQUE ;
+
+INSERT INTO Student VALUES(001,'ARUN','IT',98,'arun@gmail.com','98854848')
